76b3e1ce79964bccb6983838ddb0c48f48011f84
陌溪_ <xzx19950624@qq.com>
2018-09-28 22:13:31 +0800

    解决博客编辑和添加失败的BUG

--- vue_mogu_admin/src/utils/webUtils.js
+++ vue_mogu_admin/src/utils/webUtils.js
@@ -9,3 +9,21 @@ export function formatData(arr) {
   }
   return params
 }
+
+export function formatDataToJson(arr) {
+  var params = "["  
+  for (var key in arr) {
+    params = params + key + ":" + arr[key] + ","
+  }
+  params += "]"
+  return params
+}
+
+export function formatDataToForm(arr) {
+  const params = new FormData() 
+  for (var key in arr) {
+    console.log('"' + key + '"', arr[key])
+    params.append('"' + key + '"', arr[key])
+  }
+  return params
+}
