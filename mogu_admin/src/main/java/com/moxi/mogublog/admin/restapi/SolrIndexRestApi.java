9029dfff08a5a81469678dcda24b7c81bdcec2f8
limboy <404032194@qq.com>
2018-09-29 19:13:00 +0800

    修正类名SolrIndexService

--- mogu_admin/src/main/java/com/moxi/mogublog/admin/restapi/SolrIndexRestApi.java
+++ mogu_admin/src/main/java/com/moxi/mogublog/admin/restapi/SolrIndexRestApi.java
@@ -1,13 +1,10 @@
 package com.moxi.mogublog.admin.restapi;
 
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
