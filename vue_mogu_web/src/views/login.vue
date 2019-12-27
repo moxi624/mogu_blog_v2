0813550be34b94faa63bcc0d0f64c622c12361f6
陌溪_ <xzx19950624@qq.com>
2019-12-27 21:06:19 +0800

    add login.vue test

---
+++ vue_mogu_web/src/views/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <LoginBox></LoginBox>
+  </div>
+</template>
+
+<script>
+  import LoginBox from "../components/LoginBox";
+  export default {
+    name: "login",
+    components: {
+      LoginBox,
+    },
+    created() {
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
