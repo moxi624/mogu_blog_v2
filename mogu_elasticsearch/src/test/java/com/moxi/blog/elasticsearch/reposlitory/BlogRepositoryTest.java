74b436ff261a62f5af699707cee42b08c2c9b309
TC-zerol <tao9can8@gmail.com>
2020-01-13 10:47:33 +0800

    触发actions

--- mogu_elasticsearch/src/test/java/com/moxi/blog/elasticsearch/reposlitory/BlogRepositoryTest.java
+++ mogu_elasticsearch/src/test/java/com/moxi/blog/elasticsearch/reposlitory/BlogRepositoryTest.java
@@ -11,7 +11,6 @@
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 //import org.springframework.test.context.junit4.SpringRunner;
-//
 //import java.text.ParseException;
 //import java.text.SimpleDateFormat;
 //import java.util.ArrayList;
