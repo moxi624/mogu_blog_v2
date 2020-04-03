ada6c40fb373942e802b5411f8d654c902c609ff
陌溪_ <xzx19950624@qq.com>
2020-04-03 19:56:58 +0800

    update SQL

--- mogu_admin/src/main/java/com/moxi/mogublog/admin/restapi/SystemConfigRestApi.java
+++ mogu_admin/src/main/java/com/moxi/mogublog/admin/restapi/SystemConfigRestApi.java
@@ -20,7 +20,6 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import sun.plugin2.message.Message;
 
 import java.util.Date;
 import java.util.List;
