6fb705ae9da7d20b6866069d5aa5bf3ca7d7832b
陌溪_ <xzx19950624@qq.com>
2020-06-15 11:03:54 +0800

    docs: update sql

--- doc/数据库脚本/mogu_picture_update.sql
+++ doc/数据库脚本/mogu_picture_update.sql
@@ -3,17 +3,17 @@ DROP TABLE IF EXISTS `t_network_disk`;
 CREATE TABLE `t_network_disk` (
   `uid` varchar(32) NOT NULL COMMENT '唯一uid',
   `admin_uid` varchar(32) NOT NULL COMMENT '管理员uid',
-  `extend_name` varchar(255) DEFAULT NULL,
-  `file_name` varchar(255) DEFAULT NULL,
-  `file_path` varchar(255) DEFAULT NULL,
-  `file_size` bigint(20) NOT NULL,
-  `file_url` varchar(255) DEFAULT NULL,
-  `is_dir` int(11) NOT NULL,
-  `timestamp_name` varchar(255) DEFAULT NULL,
-  `upload_time` varchar(255) DEFAULT NULL,
+  `extend_name` varchar(255) DEFAULT NULL COMMENT '扩展名',
+  `file_name` varchar(255) DEFAULT NULL COMMENT '文件名',
+  `file_path` varchar(255) DEFAULT NULL COMMENT '文件路径',
+  `file_size` bigint(20) NOT NULL COMMENT '文件大小',
+  `is_dir` int(11) NOT NULL COMMENT '是否目录',
   `status` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '状态',
   `create_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
   `update_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '更新时间',
+  `local_url` varchar(255) DEFAULT NULL COMMENT '本地文件URL',
+  `qi_niu_url` varchar(255) DEFAULT NULL COMMENT '七牛云URL',
+  `file_old_name` varchar(255) DEFAULT NULL COMMENT '上传前文件名',
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='网盘文件表';
 
