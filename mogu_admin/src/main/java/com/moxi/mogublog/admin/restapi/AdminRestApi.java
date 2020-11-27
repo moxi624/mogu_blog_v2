43f2a1b52f03fbbd43c8f12f4464df467dd5d97b
陌溪_ <xzx19950624@qq.com>
2020-11-27 16:11:19 +0800

    fix: 修改actions，将镜像提交到Aliyun

--- mogu_admin/src/main/java/com/moxi/mogublog/admin/restapi/AdminRestApi.java
+++ mogu_admin/src/main/java/com/moxi/mogublog/admin/restapi/AdminRestApi.java
@@ -19,6 +19,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * 管理员表 RestApi
