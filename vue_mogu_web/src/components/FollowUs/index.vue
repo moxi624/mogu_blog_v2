d6ed1654309c888686c513fd851e4da9e0a6f846
陌溪_ <xzx19950624@qq.com>
2021-03-18 21:11:10 +0800

    fix: 解决门户页导航栏数量过多显示异常的问题；优化关于我们组件存在硬编码的情况；

--- vue_mogu_web/src/components/FollowUs/index.vue
+++ vue_mogu_web/src/components/FollowUs/index.vue
@@ -3,10 +3,10 @@
       <h2 class="hometitle">关注我们 么么哒！</h2>
       <ul>
         <!-- <li class="sina"><a href="/" target="_blank"><span>新浪微博</span>蘑菇博客</a></li>         -->
-        <li class="qqGroup" v-if="contact.qqGroup"><a href="//shang.qq.com/wpa/qunwpa?idkey=88bc57d77601a3c5ae97fe6d9c0bfa25c2ae166d8f0b9f6da6f7294097be6d08" target="_blank"><span>QQ群</span>{{contact.qqGroup}}</a></li>
-        <li class="qq" v-if="contact.qqNumber"><a href="tencent://AddContact/?fromId=50&fromSubId=1&subcmd=all&uin=1595833114" target="_blank"><span>QQ号</span>{{contact.qqNumber}}</a></li>
-        <li class="email" v-if="contact.email"><a :href="mailto" target="_blank"><span>邮箱帐号</span>{{contact.email}}</a></li>
-        <li class="wxgzh" v-if="contact.weChat"><a href="/" target="_blank"><span>微信号</span>{{contact.weChat}}</a></li>
+        <li class="qqGroup" v-if="contact.qqGroup"><a href="javascript:void(0);"><span>QQ群</span>{{contact.qqGroup}}</a></li>
+        <li class="qq" v-if="contact.qqNumber"><a :href="'tencent://AddContact/?fromId=50&fromSubId=1&subcmd=all&uin=' + contact.qqNumber" target="_blank"><span>QQ号</span>{{contact.qqNumber}}</a></li>
+        <li class="email" v-if="contact.email"><a href="javascript:void(0);"><span>邮箱帐号</span>{{contact.email}}</a></li>
+        <li class="wxgzh" v-if="contact.weChat"><a href="javascript:void(0);"><span>微信号</span>{{contact.weChat}}</a></li>
         <li class="github" v-if="contact.github"><a :href="contact.github" target="_blank"><span>Github</span>{{contact.github}}</a></li>
         <li class="gitee" v-if="contact.gitee"><a :href="contact.gitee" target="_blank"><span>Gitee</span>{{contact.gitee}}</a></li>
         <!-- <li class="wx"><img src="../../../static/images/wx.jpg"></li> -->
@@ -23,7 +23,6 @@ export default {
   data() {
     return {
       contact: {},
-      mailto: "",
       isShow: false
     };
   },
@@ -43,8 +42,6 @@ export default {
         if(showList.length > 2) {
           this.isShow = true;
         }
-
-        this.mailto = "mailto:" + this.contact.email;
       } else {
         getWebConfig().then(response => {
           if (response.code == this.$ECode.SUCCESS) {
