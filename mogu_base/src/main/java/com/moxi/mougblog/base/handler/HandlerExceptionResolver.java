fc39b1605ef13349230f58f21ee99811f08867ef
zflzqy <1396954535@qq.com>
2023-03-10 16:16:39 +0800

    fix 优化代码

--- mogu_base/src/main/java/com/moxi/mougblog/base/handler/HandlerExceptionResolver.java
+++ mogu_base/src/main/java/com/moxi/mougblog/base/handler/HandlerExceptionResolver.java
@@ -1,10 +1,12 @@
 package com.moxi.mougblog.base.handler;
 
+import cn.hutool.core.util.StrUtil;
 import com.moxi.mogublog.utils.ResultUtil;
 import com.moxi.mougblog.base.exception.exceptionType.*;
 import com.moxi.mougblog.base.global.BaseMessageConf;
 import com.moxi.mougblog.base.global.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,7 +33,7 @@ public class HandlerExceptionResolver implements org.springframework.web.servlet
      * @return ModelAndView
      */
     @Override
-    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) {
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, @NotNull Exception exception) {
         log.error("系统统一异常处理：", exception);
         // 若响应已响应或已关闭，则不操作
         if (response.isCommitted()) {
@@ -40,42 +42,34 @@ public class HandlerExceptionResolver implements org.springframework.web.servlet
 
         // 组装错误提示信息
         String errorCode = ErrorCode.ERROR;
-        String message = BaseMessageConf.OPERATION_FAIL;
+
+        // 获取异常说明
+        String message = exception.getMessage();
 
         // 自定义业务相关异常
         if (exception instanceof BusinessException) {
             errorCode = ((BusinessException) exception).getCode();
-            message = exception.getMessage();
         } else if (exception instanceof ApiInvalidParamException) {
             // 自定义参数校验相关的异常
             // 自动拼接异常信息
             errorCode = ErrorCode.PARAM_INCORRECT;
-            message = exception.getMessage();
         } else if (exception instanceof LoginException) {
             // 自定义登录相关的异常
             errorCode = ((LoginException) exception).getCode();
-            message = exception.getMessage();
         } else if (exception instanceof QueryException) {
             // 自定义查询相关的异常
             errorCode = ((QueryException) exception).getCode();
-            message = exception.getMessage();
         } else if (exception instanceof UpdateException) {
             // 自定义更新操作相关的异常
             errorCode = ((UpdateException) exception).getCode();
-            message = exception.getMessage();
         } else if (exception instanceof InsertException) {
             // 自定义新增操作相关的异常
             errorCode = ((InsertException) exception).getCode();
-            message = exception.getMessage();
         } else if (exception instanceof DeleteException) {
             // 自定义删除操作相关的异常
             errorCode = ((DeleteException) exception).getCode();
-            message = exception.getMessage();
-        } else if (exception instanceof Exception) {
-            // 其它异常
-            message = exception.getMessage();
-            log.error(exception.getMessage());
         }
+
 
         // 响应类型设置
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
